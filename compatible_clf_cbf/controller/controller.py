Added functionality to QuadraticFunction that greatly simplifies QuadraticLyapunov and QuadraticBarrier definitions. Fixed rotation visualization os ellipses in rviz.

compatible_clf_cbf/controller/controller.py
@@ -12,13 +12,13 @@ class QPController():
 
         # Initialize active CLF
         self._clf = clf
-        self.Hv = self._clf.A
-        self.x0 = self._clf.minimum
+        self.Hv = self._clf.hessian_matrix
+        self.x0 = self._clf.critical_point
 
         # Initialize active CBF
         self._cbf = cbf
-        self.Hh = self._cbf.A
-        self.p0 = self._cbf.minimum
+        self.Hh = self._cbf.hessian_matrix
+        self.p0 = self._cbf.critical_point
 
         # Dimensions and system model initialization
         self.state_dim = self._plant.state_dim
@@ -166,11 +166,16 @@ class QPController():
             poly_term = np.polymul( W[:,k], EYE[k,:] )
             self.num_poly = np.polyadd(self.num_poly, poly_term)
 
+        # Computes polynomial roots
+        self.pencil_char_roots = np.polynomial.polynomial.polyroots(self.pencil_char)
+        self.num_roots = np.polynomial.polynomial.polyroots(self.num_poly)
+        self.den_roots = np.polynomial.polynomial.polyroots(self.den_poly)
+
         # Computes f(0)
         self.f0 = self.num_poly[0]/pow(self.pencil_char[0],2)
 
         # Computes the numerator and denominator second derivatives for degenerate cases (L'Hopital's rule) 
-        dpencil_char = np.polyder(self.pencil_char)
-        ddpencil_char = np.polyder(dpencil_char)
-        self.LHopital_num = np.polyder(np.polyder(self.num_poly))
-        self.LHopital_den = 2*np.polyadd( np.polymul(dpencil_char,dpencil_char) , np.polymul(self.pencil_char,ddpencil_char) )
+        # dpencil_char = np.polyder(self.pencil_char)
+        # ddpencil_char = np.polyder(dpencil_char)
+        # self.LHopital_num = np.polyder(np.polyder(self.num_poly))
+        # self.LHopital_den = 2*np.polyadd( np.polymul(dpencil_char,dpencil_char) , np.polymul(self.pencil_char,ddpencil_char) )

compatible_clf_cbf/dynamic_systems/dynamic_systems.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sage.all import *
+from sage.combinat.sloane_functions import A109814
 from sage.symbolic.function import BuiltinFunction
 from sage.calculus.var import *
 
@@ -104,26 +105,83 @@ class LinearSystem(NonlinearSystem):
 
 class QuadraticFunction(BuiltinFunction):
 
-    def __init__(self, var_str, A, b, c):
+    def __init__(self, var_str, A, b, c, *types):
         BuiltinFunction.__init__(self, 'Quadratic Function', nargs=1)
 
         self._var = vector(var(var_str+','))
         self._dimension = np.size(self._var)
 
-        self.set_param(A,b,c)
+        # Configure type: 'default' = x'Ax + b'x + c
+        #                 'factored' = (x - b)'A(x-b) + c
+        if len(types) == 0:
+            self.type = 'default'
+        else:
+            for type in types:
+                if type == 'default':
+                    self.type = 'default'
+                elif type == 'factored':
+                    self.type = 'factored'
 
+        self.set_param(A,b,c)
         self._gen_var_str()
         self._create_dictionary()
 
     def set_param(self, A, b, c):
-        self.A = A
-        self.b = b
-        self.c = c
+
+        if self.type == 'default':
+            self.A = A
+            self.b = b
+            self.c = c
+        else:
+            self.hessian_matrix = A
+            self.critical_point = b
+            self.height = c
+            self.A = self.hessian_matrix
+            self.b = -(self.hessian_matrix + self.hessian_matrix.T).dot(self.critical_point)
+            self.c = self.height + self.hessian_matrix.dot(self.critical_point).dot(self.critical_point)
 
         A_symb, b_symb = matrix(self.A), vector(self.b)
         self._function = self._var * ( A_symb * self._var ) + b_symb * self._var + self.c
         self._gradient = self._function.gradient()
         self._hessian = self._function.hessian()
+
+    # Define A, b, c for Quadratics of type 'default' = x'Ax + b'x + c
+    def set_A(self, A):
+        if self.type == 'factored':
+            return
+        else:
+            self.set_param(A, self.b, self.c)
+
+    def set_b(self, b):
+        if self.type == 'factored':
+            return
+        else:
+            self.set_param(self.A, b, self.c)
+
+    def set_c(self, c):
+        if self.type == 'factored':
+            return
+        else:
+            self.set_param(self.A, self.b, c)
+
+    # Define hessian, critical point and height for Quadratics of type 'factored' = (x - b)'A(x-b) + c
+    def set_hessian(self, H):
+        if self.type == 'default':
+            return
+        else:
+            self.set_param(H, self.critical_point, self.height)
+
+    def set_critical(self, x0):
+        if self.type == 'default':
+            return
+        else:
+            self.set_param(self.hessian_matrix, x0, self.height)
+
+    def set_height(self, height):
+        if self.type == 'default':
+            return
+        else:
+            self.set_param(self.hessian_matrix, self.critical_point, height)
 
     def _gen_var_str(self):
         self._var_str = list()
@@ -181,38 +239,27 @@ class QuadraticFunction(BuiltinFunction):
 
         return symm_basis
 
+    @staticmethod
+    def rot2D(theta):
+        c, s = np.cos(theta), np.sin(theta)
+        R = np.array(((c,-s),(s,c)))
+        return R
+
+    @staticmethod
+    def canonical2D(eigen, theta):
+        Diag = np.diag(eigen)
+        R = QuadraticFunction.rot2D(theta)
+        H = np.matmul(R, np.matmul(Diag, R.T))
+        return H
+
+
 class QuadraticLyapunov(QuadraticFunction):
 
     def __init__(self, var_str, H, x0):
-
-        self.minimum = x0
-        self.A = H
-        self.b = -2*self.A.T.dot(self.minimum)
-        self.c = self.A.dot(self.minimum).dot(self.minimum)
-        
-        QuadraticFunction.__init__(self, var_str, self.A, self.b, self.c)
-
-    def set_hessian(self, H):
-        A = H
-        b = -2*self.A.T.dot(self.minimum)
-        c = self.A.dot(self.minimum).dot(self.minimum)
-        self.set_param(A,b,c)
+        QuadraticFunction.__init__(self, var_str, H, x0, 0.0, 'factored')
 
 
 class QuadraticBarrier(QuadraticFunction):
 
     def __init__(self, var_str, H, x0):
-
-        self.minimum = x0
-        self.A = H
-        self.b = -2*self.A.T.dot(self.minimum)
-        self.c = self.A.dot(self.minimum).dot(self.minimum) - 1
-        
-        QuadraticFunction.__init__(self, var_str, self.A, self.b, self.c)
-
-    def set_hessian(self, H):
-        A = H
-        b = -2*self.A.T.dot(self.minimum)
-        c = self.A.dot(self.minimum).dot(self.minimum) - 1
-        self.set_param(A,b,c)
-
+        QuadraticFunction.__init__(self, var_str, H, x0, -1.0, 'factored')

compatible_clf_cbf/graphical_simulation/graphical_simulation.py
@@ -109,13 +109,13 @@ class GraphicalSimulation():
         self._clf_marker.color.r = 0.0
         self._clf_marker.color.g = 0.4
         self._clf_marker.color.b = 1.0
-        self._clf_marker.pose.position.x = self._clf.minimum[0]
-        self._clf_marker.pose.position.y = self._clf.minimum[1]
+        self._clf_marker.pose.position.x = self._clf.critical_point[0]
+        self._clf_marker.pose.position.y = self._clf.critical_point[1]
         self._clf_marker.pose.position.z = 0
         self._clf_marker.pose.orientation.x = 0.0
         self._clf_marker.pose.orientation.y = 0.0
-        self._clf_marker.pose.orientation.z = np.sin(self.clf_angle/2)
-        self._clf_marker.pose.orientation.w = np.cos(self.clf_angle/2)
+        self._clf_marker.pose.orientation.z = np.sin(-self.clf_angle/2)
+        self._clf_marker.pose.orientation.w = np.cos(-self.clf_angle/2)
 
         # Initialize CBF marker
         self.cbf_lambda, self.cbf_angle = self._cbf.compute_eig()
@@ -130,13 +130,13 @@ class GraphicalSimulation():
         self._cbf_marker.color.r = 0.0
         self._cbf_marker.color.g = 1.0
         self._cbf_marker.color.b = 0.0
-        self._cbf_marker.pose.position.x = self._cbf.minimum[0]
-        self._cbf_marker.pose.position.y = self._cbf.minimum[1]
+        self._cbf_marker.pose.position.x = self._cbf.critical_point[0]
+        self._cbf_marker.pose.position.y = self._cbf.critical_point[1]
         self._cbf_marker.pose.position.z = 0
         self._cbf_marker.pose.orientation.x = 0.0
         self._cbf_marker.pose.orientation.y = 0.0
-        self._cbf_marker.pose.orientation.z = np.sin(self.cbf_angle/2)
-        self._cbf_marker.pose.orientation.w = np.cos(self.cbf_angle/2)
+        self._cbf_marker.pose.orientation.z = np.sin(-self.cbf_angle/2)
+        self._cbf_marker.pose.orientation.w = np.cos(-self.cbf_angle/2)
 
     def draw_trajectory(self, point):
         new_trajectory_point = Point()

examples/compatibility_test.py
@@ -1,10 +1,11 @@
+from sage.symbolic.constants import Pi
 import rospy
 import numpy as np
-
+from scipy.spatial.transform import Rotation as R
 from compatible_clf_cbf.controller import QPController
 from compatible_clf_cbf.dynamic_simulation import SimulateDynamics
 from compatible_clf_cbf.graphical_simulation import GraphicalSimulation
-from compatible_clf_cbf.dynamic_systems import AffineSystem, QuadraticLyapunov, QuadraticBarrier
+from compatible_clf_cbf.dynamic_systems import AffineSystem, QuadraticLyapunov, QuadraticBarrier, QuadraticFunction
     
 # Simulation parameters
 dt = .002
@@ -31,10 +32,13 @@ x0 = np.array([0,0])
 clf = QuadraticLyapunov(state_string, Hv, x0)
 
 # Create CBF
-xaxis_length, yaxis_length = 1.0, 1.0
+xaxis_length = 1.0
+yaxis_length = 1.0
+obs_angle = Pi/10
 lambda1, lambda2 = 1/xaxis_length**2, 1/yaxis_length**2
-Hh = np.array([ [ lambda1 , 0.0 ],
-                [ 0.0 , lambda2 ] ])
+Sigma_h = np.array([ [ lambda1 , 0.0 ],
+                     [ 0.0 , lambda2 ] ])
+R = QuadraticFunction.rot2D(obs_angle)
 p0 = np.array([0,1])
 cbf = QuadraticBarrier(state_string, Hh, p0)
 
@@ -42,9 +46,9 @@ cbf = QuadraticBarrier(state_string, Hh, p0)
 ref = np.array([0,0])
 qp_controller = QPController(plant, clf, cbf, gamma = 1.0, alpha = 1.0, p = 10.0)
 
-# print(np.polynomial.polynomial.polyroots(qp_controller.pencil_char))
-# print(np.polynomial.polynomial.polyroots(qp_controller.num_poly))
-# print(qp_controller.LHopital_den)
-# print(qp_controller.LHopital_num)
+n = 2
+print("Basis of R"+str(n)+":")
+basis = QuadraticFunction.symmetric_basis(n)
+for i in range(np.size(basis,0)):
+    print(basis[0])
 
-basis = qp_controller._clf.symmetric_basis(3)

examples/rviz_example.py
@@ -1,4 +1,7 @@
+from compatible_clf_cbf.dynamic_systems.dynamic_systems import QuadraticFunction
+from sage.symbolic.constants import Pi
 import rospy
+import math
 import numpy as np
 
 from compatible_clf_cbf.controller import QPController
@@ -22,22 +25,27 @@ try:
     plant = AffineSystem(state_string, control_string, f, *g)
 
     # Define initial state for plant simulation
-    x_init, y_init = 0.1, 5
+    x_init, y_init = -2.0, 5
     initial_state = np.array([x_init,y_init])
 
     # Create CLF
-    lambda_x, lambda_y = 3.0, 1.0
-    Hv = np.array([ [ lambda_x , 0.0 ],
-                    [ 0.0 , lambda_y ] ])
+    lambdav_x, lambdav_y = 1.0, 2.0
+    CLFangle = math.pi/4
     x0 = np.array([0,0])
+
+    CLFeigen = np.array([ lambdav_x , lambdav_y ])
+    Hv = QuadraticFunction.canonical2D(CLFeigen, CLFangle)
     clf = QuadraticLyapunov(state_string, Hv, x0)
 
     # Create CBF
-    xaxis_length, yaxis_length = 1.0, 1.0
-    lambda1, lambda2 = 1/xaxis_length**2, 1/yaxis_length**2
-    Hh = np.array([ [ lambda1 , 0.0 ],
-                    [ 0.0 , lambda2 ] ])
+    xaxis_length, yaxis_length = 1.0, 2.0
+    CBFangle = -math.pi/4
     p0 = np.array([0,3])
+
+    lambdah_x, lambdah_y = 1/xaxis_length**2, 1/yaxis_length**2
+    CBFeigen = np.array([ lambdah_x , lambdah_y ])
+    Hh = QuadraticFunction.canonical2D(CBFeigen, CBFangle)
+    print(Hh)
     cbf = QuadraticBarrier(state_string, Hh, p0)
 
     # Create QP controller