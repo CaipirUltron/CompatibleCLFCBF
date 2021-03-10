Fixed the Faddeev-LeVerrier algorithm for computation of the numerator polynomial. Added visualization function for plotting f(\lambda). Code is now computing the correct f(\lambda) (same as MATLAB).

compatible_clf_cbf/controller/controller.py
@@ -326,8 +326,9 @@ class QPController():
             return
 
         # Computes the pencil characteristic polynomial and denominator of f(\lambda)
-        self.pencil_char = ( self.detHv/np.real(np.prod(self.pencil_char_roots)) ) * np.real(np.polynomial.polynomial.polyfromroots(self.pencil_char_roots))
-        self.den_poly = np.polymul(self.pencil_char, self.pencil_char)
+        pencil_det = np.real(np.prod(self.pencil_char_roots))
+        self.pencil_char = ( self.detHv/pencil_det ) * np.real(np.polynomial.polynomial.polyfromroots(self.pencil_char_roots))
+        self.den_poly = np.polynomial.polynomial.polymul(self.pencil_char, self.pencil_char)
 
         # This computes the pencil adjugate expansion and the set of numerator vectors by the adapted Faddeev-LeVerrier algorithm.
         D = np.zeros([n, n, n])
@@ -337,40 +338,54 @@ class QPController():
         Omega[0,:] = D[:][:][0].dot(self.v0)
 
         for k in range(1,n):
-                D[:][:][k] = Hv_inv * ( self.Hh * D[:][:][k-1] - self.pencil_char[k]*np.eye(n) )
-                Omega[k,:] = D[:][:][k].dot(self.v0)        
+            D[:][:][k] = np.matmul( Hv_inv, np.matmul(self.Hh, D[:][:][k-1]) - self.pencil_char[k]*np.eye(n) )
+            Omega[k,:] = D[:][:][k].dot(self.v0)
 
         # Computes the numerator polynomial
         W = np.zeros([n,n])
         for i in range(n):
             for j in range(n):
-                W[i,j] = self.Hh.dot(Omega[i,:]).dot(Omega[j,:])
+                W[i,j] = np.inner(self.Hh.dot(Omega[i,:]), Omega[j,:])
 
         EYE = np.eye(n)
-        self.num_poly = np.zeros(n+1)
+        self.num_poly = np.polynomial.polynomial.polyzero
         for k in range(n):
-            poly_term = np.polymul( W[:,k], EYE[k,:] )
-            self.num_poly = np.polyadd(self.num_poly, poly_term)
+            poly_term = np.polynomial.polynomial.polymul( W[:,k], EYE[:,k] )
+            self.num_poly = np.polynomial.polynomial.polyadd(self.num_poly, poly_term)
 
         # Computes polynomial roots
         self.num_roots = np.polynomial.polynomial.polyroots(self.num_poly)
         self.den_roots = np.polynomial.polynomial.polyroots(self.den_poly)
 
         # Computes f(0)
-        self.f0 = self.num_poly[0]/pow(self.pencil_char[0],2)
+        self.f0 = self.num_poly[0]/(self.pencil_char[0]**2)
 
         # Computes critical points
         dnum_poly = np.polynomial.polynomial.polyder(self.num_poly)
         dpencil_char = np.polynomial.polynomial.polyder(self.pencil_char)
 
-        num_df = np.polysub( np.polymul(dnum_poly, self.pencil_char), 2*np.polymul(self.num_poly, dpencil_char) )
-        self.critical_points = np.polynomial.polynomial.polyroots(num_df)
+        poly1 = np.polynomial.polynomial.polymul(dnum_poly, self.pencil_char)
+        poly2 = 2*np.polynomial.polynomial.polymul(self.num_poly, dpencil_char)
+        num_df = np.polynomial.polynomial.polysub( poly1, poly2 )
+        critical_points = np.polynomial.polynomial.polyroots(num_df)
+        self.critical_points = np.real(np.extract( critical_points.imag == 0.0, critical_points ))
 
         num_critical = len(self.critical_points)
         self.critical_values = []
         for k in range(num_critical):
-            if self.critical_points[k].imag == 0.0:
-                num_value = np.polynomial.polynomial.polyval( self.critical_points[k], self.num_poly )
-                pencil_char_value = np.polynomial.polynomial.polyval( self.critical_points[k], self.pencil_char )
-                f_value = num_value/(pencil_char_value**2)
-                self.critical_values.append(f_value.real)
+            num_value = np.polynomial.polynomial.polyval( self.critical_points[k], self.num_poly )
+            pencil_char_value = np.polynomial.polynomial.polyval( self.critical_points[k], self.pencil_char )
+            f_value = num_value/(pencil_char_value**2)
+            self.critical_values.append(f_value.real)
+
+    # Returns the values of f at t
+    def fvalues(self, t):
+
+        numpoints = len(t)
+        fvalues = np.zeros(numpoints)
+        for k in range(numpoints):
+            num_value = np.polynomial.polynomial.polyval( t[k], self.num_poly )
+            pencil_char_value = np.polynomial.polynomial.polyval( t[k], self.pencil_char )
+            fvalues[k] = num_value/(pencil_char_value**2)
+
+        return fvalues

compatible_clf_cbf/dynamic_systems/dynamic_systems.py
@@ -243,14 +243,32 @@ class QuadraticFunction(BuiltinFunction):
         if self._dimension != 2:
             return
 
-        def parameterize_ellipse(delta):
-
-            y1 = np.sqrt(delta/eig[0])*np.sin(t)
-            y2 = np.sqrt(delta/eig[1])*np.cos(t)
-
+        def change_variables(y1, y2):
             p = self.critical_point
             x = p[0] + Q[0,0]*y1 + Q[0,1]*y2
             y = p[1] + Q[1,0]*y1 + Q[1,1]*y2
+            return x, y
+
+        def parameterize_ellipse(delta):
+            y1 = np.sqrt(delta/eig[0])*np.sin(t)
+            y2 = np.sqrt(delta/eig[1])*np.cos(t)
+            return y1, y2
+
+        def parameterize_hyperbola(delta):
+            # y1 = np.sqrt(np.abs(delta/eig[0]))*(1/np.cos(t))
+            # y2 = np.sqrt(np.abs(delta/eig[1]))*np.tan(t)
+            y1 = np.sqrt(np.abs(delta/eig[0]))*np.cosh(t)
+            y2 = np.sqrt(np.abs(delta/eig[1]))*np.sinh(t)
+            return y1, y2
+
+        def parameterize(delta):
+
+            if level_type == 'ellipse':
+                y1, y2 = parameterize_ellipse(delta)
+            elif level_type == 'hyperbola':
+                y1, y2 = parameterize_hyperbola(delta)
+
+            x, y = change_variables(y1, y2)
 
             u, v = np.zeros(numpoints), np.zeros(numpoints)
             for k in range(numpoints):
@@ -260,28 +278,45 @@ class QuadraticFunction(BuiltinFunction):
 
             return x, y, u, v
 
-        t = np.linspace(0, 2*math.pi, numpoints)
+        t = np.linspace(-math.pi, math.pi, numpoints)
 
+        # Parameterize 
         eig, Q = np.linalg.eig(self.hessian_matrix)
         if eig[0]*eig[1] > 0:
             # ellipse
+            level_type = 'ellipse'
             if eig[0]>0:
                 # convex
                 delta = C-self.height
                 if delta > 0:
-                    x, y, u, v = parameterize_ellipse(delta)
+                    x, y, u, v = parameterize(delta)
                 else:
                     x, y, u, v = [], [], [], []
             else:
                 # concave
                 delta = self.height-C
                 if delta > 0:
-                    x, y, u, v = parameterize_ellipse(delta)
+                    x, y, u, v = parameterize(delta)
                 else:
-                    x, y = [], [], [], []
+                    x, y, u, v = [], [], [], []
         elif eig[0]*eig[1] < 0:
             # hyperbola
-            x, y, u, v = [], [], [], []
+            level_type = 'hyperbola'
+            bareig1 = eig[0]
+            if eig[0]>0:
+                # convex
+                delta = C-self.height
+                if delta > 0:
+                    x, y, u, v = parameterize(delta)
+                else:
+                    x, y, u, v = [], [], [], []
+            else:
+                # concave
+                delta = self.height-C
+                if delta > 0:
+                    x, y, u, v = parameterize(delta)
+                else:
+                    x, y, u, v = [], [], [], []
         else:
             x, y, u, v = [], [], [], []
 

examples/compatibility_test.py
@@ -1,9 +1,10 @@
-import rospy
+
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+
 from compatible_clf_cbf.controller import QPController, QuadraticProgram
-from compatible_clf_cbf.dynamic_simulation import SimulateDynamics
-from compatible_clf_cbf.graphical_simulation import GraphicalSimulation
 from compatible_clf_cbf.dynamic_systems import AffineSystem, QuadraticLyapunov, QuadraticBarrier, QuadraticFunction
     
 # Simulation parameters
@@ -12,54 +13,51 @@ sim_freq = 1/dt
 T = 20
 
 # Define 2D plant and initial state
-n = 3
-f = ['0','0','0']
-g1 = ['1','0','0']
-g2 = ['0','1','0']
-g3 = ['0','0','1']
-g = [g1,g2,g3]
-state_string = 'x1, x2, x3'
-control_string = 'u1, u2, u3'
+f = ['0','0']
+g1 = ['1','0']
+g2 = ['0','1']
+g = [g1,g2]
+state_string = 'x1, x2, '
+control_string = 'u1, u2, '
 plant = AffineSystem(state_string, control_string, f, *g)
 
-# Create random CLF and CBF
-Hv = np.zeros([n,n])
-Hh = np.zeros([n,n])
-basis = QuadraticFunction.symmetric_basis(n)
-for k in range(np.size(basis,0)):
-    Hv = Hv + (2*np.random.rand())*basis[k]
-    Hh = Hh + (2*np.random.rand())*basis[k]
+# Define initial state for plant simulation
+x_init, y_init = 3.1, 5
+initial_state = np.array([x_init,y_init])
 
-eigHv, _ = np.linalg.eig(Hv)
-eigHh, _ = np.linalg.eig(Hh)
+# Create CLF
+lambdav_x, lambdav_y = 3.0, 1.0
+CLFangle = 0.0
+x0 = np.array([0,0])
 
-# print("Eigenvalues of Hv:" + str(eigHv))
-# print("Eigenvalues of Hh:" + str(eigHh))
-
-rng = 10
-x0 = rng*( 2*np.random.rand(n) - np.ones(n) )
-p0 = rng*( 2*np.random.rand(n) - np.ones(n) )
-
+CLFeigen = np.array([ lambdav_x , lambdav_y ])
+Hv = QuadraticFunction.canonical2D(CLFeigen, CLFangle)
 clf = QuadraticLyapunov(state_string, Hv, x0)
+
+# Create CBF
+xaxis_length, yaxis_length = 2.0, 1.0
+CBFangle = -math.pi/10
+p0 = np.array([3,3])
+
+lambdah_x, lambdah_y = 1/xaxis_length**2, 1/yaxis_length**2
+CBFeigen = np.array([ lambdah_x , lambdah_y ])
+Hh = QuadraticFunction.canonical2D(CBFeigen, CBFangle)
 cbf = QuadraticBarrier(state_string, Hh, p0)
 
 # Create QP controller
 qp_controller = QPController(plant, clf, cbf, gamma = [1.0, 1.0], alpha = [1.0, 1.0], p = [10.0, 10.0])
 
+print("Numerator roots = " + str(qp_controller.num_poly))
 print("Pencil eigenvalues:" + str(qp_controller.pencil_char_roots))
 
-print("det(H(\lambda)) = " + str(qp_controller.pencil_char))
+print("Critical:" + str(qp_controller.critical_points))
+print("Critical values:" + str(qp_controller.critical_values))
 
-# print("Zeros = " + str(qp_controller.num_roots))
-# print("Poles = " + str(qp_controller.den_roots))
-
-# print("Hv = " + str(clf.hessian()))
-# print("Hh = " + str(cbf.hessian()))
-
-P = np.eye(2)
-q = np.zeros(2)
-A = np.array([[1.0, 1.0]])
-b = np.array([-1.0])
-
-innerQP = QuadraticProgram(P=P, q=q)
-print("QP solution is " + str(innerQP.get_solution()))
+fig = plt.figure()
+axes_lim = (0, 10.0, 0, 300.0)
+ax = plt.axes(xlim=axes_lim[0:2], ylim=axes_lim[2:4])
+ax.set_title('CLF-CBF QP-based Control')
+lamb = np.arange(axes_lim[0], axes_lim[1], 0.01)
+fvalues = qp_controller.fvalues(lamb)
+ax.plot(lamb, fvalues, zorder=100, color='red')
+plt.show()

examples/matplot_example.py
@@ -25,12 +25,12 @@ control_string = 'u1, u2, '
 plant = AffineSystem(state_string, control_string, f, *g)
 
 # Define initial state for plant simulation
-x_init, y_init = 0.1, 5
+x_init, y_init = 3.1, 5
 initial_state = np.array([x_init,y_init])
 
 # Create CLF
-lambdav_x, lambdav_y = 6.0, 1.0
-CLFangle = 0.0
+lambdav_x, lambdav_y = 1.0, 2.0
+CLFangle = math.radians(45.0)
 x0 = np.array([0,0])
 
 CLFeigen = np.array([ lambdav_x , lambdav_y ])
@@ -39,8 +39,8 @@ clf = QuadraticLyapunov(state_string, Hv, x0)
 
 # Create CBF
 xaxis_length, yaxis_length = 2.0, 1.0
-CBFangle = 0.0
-p0 = np.array([0,3])
+CBFangle = math.radians(-45.0)
+p0 = np.array([3,3])
 
 lambdah_x, lambdah_y = 1/xaxis_length**2, 1/yaxis_length**2
 CBFeigen = np.array([ lambdah_x , lambdah_y ])
@@ -48,7 +48,8 @@ Hh = QuadraticFunction.canonical2D(CBFeigen, CBFangle)
 cbf = QuadraticBarrier(state_string, Hh, p0)
 
 # Create QP controller
-qp_controller = QPController(plant, clf, cbf, gamma = [1.0, 10.0], alpha = [1.0, 1.0], p = [10.0, 10.0], init_eig = [ 1.0, 6.0 ])
+init_eig = CLFeigen
+qp_controller = QPController(plant, clf, cbf, gamma = [1.0, 10.0], alpha = [1.0, 1.0], p = [10.0, 10.0], init_eig = init_eig)
 
 # Initialize simulation object
 dynamicSimulation = SimulateDynamics(plant, initial_state)

examples/rviz_example.py
@@ -2,6 +2,8 @@
 import rospy
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 
 from compatible_clf_cbf.controller import QPController
 from compatible_clf_cbf.dynamic_simulation import SimulateDynamics
@@ -10,7 +12,7 @@ from compatible_clf_cbf.dynamic_systems import AffineSystem, QuadraticLyapunov, 
 
 try:
     # Simulation parameters
-    dt = .002
+    dt = .005
     sim_freq = 1/dt
     T = 20
 
@@ -28,7 +30,7 @@ try:
     initial_state = np.array([x_init,y_init])
 
     # Create CLF
-    lambdav_x, lambdav_y = 6.0, 1.0
+    lambdav_x, lambdav_y = 2.0, 1.0
     CLFangle = 0.0
     x0 = np.array([0,0])
 
@@ -38,7 +40,7 @@ try:
 
     # Create CBF
     xaxis_length, yaxis_length = 2.0, 1.0
-    CBFangle = 0.0
+    CBFangle = math.radians(0.0)
     p0 = np.array([0,3])
 
     lambdah_x, lambdah_y = 1/xaxis_length**2, 1/yaxis_length**2
@@ -47,7 +49,23 @@ try:
     cbf = QuadraticBarrier(state_string, Hh, p0)
 
     # Create QP controller
-    qp_controller = QPController(plant, clf, cbf, gamma = [1.0, 10.0], alpha = [1.0, 1.0], p = [10.0, 10.0], init_eig = [ 1.0, 6.0 ])
+    init_eig = [ 1.0, 2.0 ]   
+    # init_eig = CLFeigen
+    qp_controller = QPController(plant, clf, cbf, gamma = [1.0, 10.0], alpha = [1.0, 1.0], p = [10.0, 10.0], init_eig = init_eig)
+
+    # Show initial plot of f(\lambda)
+    print("Pencil eigenvalues:" + str(qp_controller.pencil_char_roots))
+    print("Critical:" + str(qp_controller.critical_points))
+    print("Critical values:" + str(qp_controller.critical_values))
+
+    fig = plt.figure()
+    axes_lim = (0, 20.0, 0, 300.0)
+    ax = plt.axes(xlim=axes_lim[0:2], ylim=axes_lim[2:4])
+    ax.set_title('CLF-CBF QP-based Control')
+    lamb = np.arange(axes_lim[0], axes_lim[1], 0.01)
+    fvalues = qp_controller.fvalues(lamb)
+    ax.plot(lamb, fvalues, zorder=100, color='red')
+    plt.show()
 
     # Initialize simulation object
     dynamicSimulation = SimulateDynamics(plant, initial_state)
@@ -65,14 +83,9 @@ try:
         qp_controller.update_clf_dynamics(lambda_control)
         control, delta = qp_controller.compute_control(state)
 
-        # print("Numerator = " + str(qp_controller.num_poly))
-        # print("Pencil characteristic = " + str(qp_controller.pencil_char))
-
-        # print("Numerator roots = " + str(qp_controller.num_poly))
-        # print("Pencil eigenvalues:" + str(qp_controller.pencil_char_roots))
-        
-        # print("Critical:" + str(qp_controller.critical_points))
-        # print("Critical values:" + str(qp_controller.critical_values))
+        print("Pencil eigenvalues:" + str(qp_controller.pencil_char_roots))
+        print("Critical:" + str(qp_controller.critical_points))
+        print("Critical values:" + str(qp_controller.critical_values))
 
         # Send actuation commands 
         dynamicSimulation.send_control_inputs(control, dt)