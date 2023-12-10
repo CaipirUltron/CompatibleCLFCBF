Curvature/stability computation fixed. NEXT STEPS: simulate different systems, with and without drift: check if stability conditions still hold with drift. Design controller for CLF optimization

common/common_methods.py
@@ -396,13 +396,20 @@ def compute_curvatures(H, normal):
 
     barH = Q.T @ H @ Q
     shape_operator = barH[1:,1:]
-    eigs = np.linalg.eigvals(shape_operator)
+    curvatures, directions_in_TpS = np.linalg.eig(shape_operator)
 
-    min_curvature = np.min(eigs)
-    max_curvature = np.max(eigs)
-    curvatures = [ min_curvature, max_curvature ]
+    basis_for_TpS = np.zeros([n,n-1])
+    for k in range(directions_in_TpS.shape[1]):
+        direction = directions_in_TpS[:,k]
 
-    return curvatures, shape_operator
+        barv = np.zeros(n)
+        barv[1:] = direction
+        basis_for_TpS[:,k] = Q @ barv
+
+        if basis_for_TpS[:,k].T @ normal > 1e-10:
+            raise Exception("Principal curvatures are not computed correctly.")
+
+    return curvatures, basis_for_TpS
 
 def rgb(minimum, maximum, value):
     '''

controllers/nominal_controller.py
@@ -4,7 +4,7 @@ from common import sat
 from dynamic_systems import Unicycle
 from quadratic_program import QuadraticProgram
 from controllers.compatibility import CLFCBFPair
-from controllers.equilibrium_algorithms import check_equilibrium
+from controllers.equilibrium_algorithms import check_invariant, check_equilibrium, compute_equilibria
 
 class NominalQP():
     '''
@@ -35,32 +35,13 @@ class NominalQP():
 
         self.ctrl_dt = dt
 
-        self.eq_dt = 0.1
+        self.eq_dt = 0.2
         self.equilibria = []
         
         self.updated_timer = False
         self.timer = 0.0
         self.last_updated_by = None
         self.last_eq_t = 0.0
-
-    # def get_equilibria(self):
-    #     '''
-    #     Computes the equilibrium points every 
-    #     '''
-    #     elapsed_time = self.timer - self.last_eq_t
-    #     if elapsed_time > self.eq_dt:
-    #         initial_guess = self.plant.get_state()
-    #         eq_sol = compute_equilibria_algorithm7( self.plant, self.clf, self.cbfs[0], initial_guess, c = self.p * self.alpha)
-    #         if eq_sol != None and eq_sol["cost"] < 1e-10:
-    #             eq_pt = np.array(eq_sol["point"])
-    #             eq_pts = np.array([ eq["point"] for eq in self.equilibria ])
-    #             if len(eq_pts) > 0:
-    #                 if np.all( np.linalg.norm(eq_pt - eq_pts, axis=1) > 1e-5 ):
-    #                     self.equilibria.append(eq_sol)
-    #             else:
-    #                 self.equilibria.append(eq_sol)
-
-    #         self.last_eq_t = self.timer
 
     def get_control(self):
         '''
@@ -81,6 +62,15 @@ class NominalQP():
         is_equilibrium, eq_pt = check_equilibrium(self.plant, self.clf, self.cbf, self.plant.get_state(), slack_gain=self.p, clf_gain=self.alpha)
         if is_equilibrium:
             print("Equilibrium point was found: " + str(eq_pt))
+
+        # elapsed_time = self.timer - self.last_eq_t
+        # is_invariant, inv_pt = check_invariant(self.plant, self.clf, self.cbf, self.plant.get_state(), slack_gain=self.p, clf_gain=self.alpha)
+        # if is_invariant and elapsed_time > self.eq_dt:
+        #     solutions, log = compute_equilibria(self.plant, self.clf, self.cbf, [inv_pt["x"]], slack_gain=self.p, clf_gain=self.alpha)
+        #     if len(solutions) > 0:
+        #         eq_pt = solutions[0]
+        #         print("Equilibrium point was found: " + str(eq_pt))
+        #     self.last_eq_t = self.timer
 
         return control
 

examples/sos_nominalQP.py
@@ -5,7 +5,7 @@ from functions import Kernel, KernelLyapunov, KernelBarrier
 from controllers import NominalQP
 from common import create_quadratic, rot2D
 
-initial_state = [0.0, 6.0]
+initial_state = [0.2, 7.0]
 initial_control = [0.0, 0.0]
 n = len(initial_state)
 m = len(initial_control)
@@ -29,7 +29,7 @@ points = []
 points += [{ "point": [ 0.0,  0.0], "level": 0.0 }]
 points += [{ "point": [ 3.0,  3.0], "level": base_level, "gradient": [ 1.0,  1.0] }]
 points += [{ "point": [-3.0,  3.0], "level": base_level, "gradient": [-1.0,  1.0] }]
-points += [{ "point": [ 0.0,  6.0],                      "gradient": [ 0.0,  1.0], "curvature": -1.5 }]
+points += [{ "point": [ 0.0,  5.0],                      "gradient": [ 0.0,  1.0], "curvature": -1.2 }]
 
 clf = KernelLyapunov(*initial_state, kernel=kernel, points=points)
 
@@ -49,9 +49,10 @@ points += [{ "point": [-2.0, -2.0], "level": 0.0, "gradient": [-1.0, -1.0] }]
 points += [{ "point": [-2.0,  2.0], "level": 0.0, "gradient": [-1.0,  1.0] }]
 points += [{ "point": [ 0.0,  2.0], "level": 0.0, "gradient": [ 0.0,  1.0] }]
 points += [{ "point": [ 0.0, -2.0], "level": 0.0, "gradient": [ 0.0, -1.0] }]
-# points += [{ "point": [ 2.0,  0.0], "level": 0.0, "gradient": [ 1.0,  0.0] }]
-# points += [{ "point": [-2.0,  0.0], "level": 0.0, "gradient": [-1.0,  0.0] }]
-displacement = 2*np.array([0,2])
+points += [{ "point": [ 2.0,  0.0], "level": 0.0 }]
+points += [{ "point": [-2.0,  0.0], "level": 0.0 }]
+
+displacement = 3*np.array([0,1])
 for pt in points:
     pt["point"] = ( np.array(pt["point"]) + displacement ).tolist()
 
@@ -59,7 +60,7 @@ cbf = KernelBarrier(*initial_state, kernel=kernel, points=points)
 # cbf = KernelBarrier(*initial_state, kernel=kernel, boundary_points=boundary_points)
 
 # ------------------------------------------------- Define controller ------------------------------------------------------
-T = 15
+T = 12
 sample_time = .002
 p, alpha, beta = 1.0, 1.0, 1.0
 controller = NominalQP(plant, clf, cbf, alpha, beta, p, dt=sample_time)

functions/functions.py
@@ -709,7 +709,13 @@ class KernelQuadratic(Function):
         self.set_param(**kwargs)
         self.evaluate()
 
-        default_plot_config = {"figsize": (5,5), "axeslim": (-6,6,-6,6), "color": mcolors.TABLEAU_COLORS['tab:green']}
+        default_color = "black"
+        if isinstance(self, KernelLyapunov):
+            default_color = mcolors.TABLEAU_COLORS['tab:blue']
+        elif isinstance(self, KernelBarrier):
+            default_color = mcolors.TABLEAU_COLORS['tab:green']
+
+        default_plot_config = {"figsize": (5,5), "axeslim": (-6,6,-6,6), "color": default_color}
         self.plot_config = default_plot_config
 
     def set_param(self, **kwargs):
@@ -767,7 +773,7 @@ class KernelQuadratic(Function):
                 self.fit(point_list)
 
         if np.shape(self.matrix_coefs) != (self.kernel_dim, self.kernel_dim):
-            raise Exception("P must be (p x p), where p is the kernel dimension!") 
+            raise Exception("P must be (p x p), where p is the kernel dimension!")
         
     def update(self, param_ctrl, dt):
         '''
@@ -809,16 +815,6 @@ class KernelQuadratic(Function):
             pt = pt_dict["point"]
             m = self.kernel.function(pt)
             Jm = self.kernel.jacobian(pt)
-
-                # if "level" in keys:
-                #     level_value = pt_dict["level"]
-                # elif self.constant > 0.0:                     # its a CBF (function is not p.s.d.) ----->>> it will have a boundary! (consider the specified level to be 0)
-                #     level_value = 0.0
-                # else:                                         # its either CLF (function is p.s.d.) or a crazy strictly positive function with C < 0
-                #     raise Exception("For a CLF, you have to specify a level set different than 0 for the point.")
-                
-                # Sums errors to objective function
-                # obj_expr += ( 0.5 * m.T @ F_var @ m - self.constant - level_value )**2
                 
             # Define point-level constraints
             if "level" in keys:
@@ -864,7 +860,12 @@ class KernelQuadratic(Function):
         fit_problem.solve()
 
         if "optimal" in fit_problem.status:
-            print("Fitting was successful with final cost = " + str(fit_problem.value) + " and message: " + str(fit_problem.status))
+            if isinstance(self, KernelLyapunov):
+                print("Lyapunov fitting was successful with final cost = " + str(fit_problem.value) + " and message: " + str(fit_problem.status))
+            elif isinstance(self, KernelBarrier):
+                print("Barrier fitting was successful with final cost = " + str(fit_problem.value) + " and message: " + str(fit_problem.status))
+            else:
+                print("Function fitting was successful with final cost = " + str(fit_problem.value) + " and message: " + str(fit_problem.status))
             self.set_param( coefficients =  F_var.value )
             return fit_problem.value
         else:
@@ -890,6 +891,21 @@ class KernelQuadratic(Function):
         '''
         self.point_list.append({ "point": point, "level": 0.0 })
         return self.fit(self.point_list)
+
+    def get_curvature(self, point):
+        '''
+        For testing only. Only works in 2D
+        '''
+        if self._dim != 2:
+            raise Exception("Not intended to work with dimensions higher than 2")
+        
+        grad = self.gradient(point)
+        grad_norm = np.linalg.norm(grad)
+        normalized_grad = grad/grad_norm
+        z = rot2D(np.pi/2) @ normalized_grad
+        Hessian = self.hessian(point)
+
+        return z.T @ Hessian @ z / grad_norm
 
     def function(self, point):
         '''
@@ -921,11 +937,17 @@ class KernelQuadratic(Function):
                 Hessian[i,j] = Jac_m_i @ self.matrix_coefs @ Jac_m_j + m @ self.matrix_coefs @ hessian_m_ij
         return Hessian
 
-    def plot(self, **kwargs):
+    def plot_level(self, **kwargs):
         '''
         Plots the kernel-based function using matplotlib
         '''
+        ax = None
+        level = 0.0
         for key in kwargs:
+            if key == "level":
+                level = kwargs["level"]
+            if key == "axes":
+                ax = kwargs["axes"]
             if key == "figsize":
                 self.plot_config["figsize"] = kwargs[key]
             if key == "axeslim":
@@ -933,8 +955,9 @@ class KernelQuadratic(Function):
             if key == "color":
                 self.plot_config["color"] = kwargs[key]
 
-        fig = plt.figure(figsize = self.plot_config["figsize"], constrained_layout=True)
-        ax = fig.add_subplot(111)
+        if ax == None:
+            fig = plt.figure(figsize = self.plot_config["figsize"], constrained_layout=True)
+            ax = fig.add_subplot(111)
 
         x_lim = self.plot_config["axeslim"][0:2]
         y_lim = self.plot_config["axeslim"][2:4]
@@ -942,7 +965,7 @@ class KernelQuadratic(Function):
         ax.set_xlim(*x_lim)
         ax.set_ylim(*y_lim)
 
-        self.contour_plot(ax, levels=[0.0], colors=self.plot_config["color"], min_lims=[ x_lim[0], y_lim[0] ], max_lims=[ x_lim[1], y_lim[1] ], resolution=0.1)
+        self.contour_plot(ax, levels=[level], colors=self.plot_config["color"], min_lims=[ x_lim[0], y_lim[0] ], max_lims=[ x_lim[1], y_lim[1] ], resolution=0.1)
         return ax
 
     def get_shape(self):

graphics/graphics_matplot.py
@@ -49,6 +49,9 @@ class Plot2DSimulation():
         # Specify main ax
         main_ax = self.plot_config["gridspec"][-1]
 
+        '''
+        I honestly don't know wtf is this
+        '''
         def order2indexes(k, m):
             i = int((k-1)/m)
             j = int(np.mod(k-1, m))
@@ -79,6 +82,9 @@ class Plot2DSimulation():
             else:
                 self.main_ax = self.fig.add_subplot(gs[i[0]:(i[-1]+1),j[0]:(j[-1]+1)])
 
+        '''
+        Configures plot
+        '''
         axes_lim = self.plot_config["axeslim"]
         self.x_lim = axes_lim[0:2]
         self.y_lim = axes_lim[2:4]
@@ -213,7 +219,7 @@ class Plot2DSimulation():
         else:
             self.runs = False
             self.animation.event_source.stop()
-
+            
         graphical_elements = []
         graphical_elements.append(self.time_text)
         graphical_elements.append(self.trajectory)

test_equilibria.py
@@ -1,37 +1,48 @@
 import sys
-import json
 import importlib
 import numpy as np
 import matplotlib.pyplot as plt
 
-from graphics import Plot2DSimulation
-from controllers.equilibrium_algorithms import compute_equilibria_algorithm9
+from common import rgb
+from controllers.equilibrium_algorithms import check_equilibrium, compute_equilibria
 
 # Load simulation file
 simulation_file = sys.argv[1].replace(".json","")
 sim = importlib.import_module("examples."+simulation_file, package=None)
 
-try:
-    with open("logs/"+simulation_file + ".json") as file:
-        print("Loading graphical simulation with "+simulation_file + ".json")
-        logs = json.load(file)
-except IOError:
-    print("Couldn't locate " + simulation_file + ".json")
+# ----------------------------------------------------- Plotting ---------------------------------------------------------
 
-# -----------------------------------Plots starting of simulation ------------------------------------------
-plotSim = Plot2DSimulation( logs, sim.plant, sim.clf, [sim.cbf], plot_config = sim.plot_config )
-plotSim.plot_frame(5.0)
+fig = plt.figure(constrained_layout=True)
+ax = fig.add_subplot(111)
+ax.set_title("Kernel-based CLF-CBF fitting")
+
+sim.clf.plot_level(axes = ax, level = 23.0, axeslim = [-10, 10, -10, 10])
+sim.cbf.plot_level(axes = ax, axeslim = [-10, 10, -10, 10])
+
+arrow_width = 0.005
+for pt in sim.clf.point_list:
+    ax.plot(pt["point"][0], pt["point"][1], 'o', color="blue")
+    if "gradient" in pt.keys():
+        if "curvature" in pt.keys():
+            color = rgb(-10.0, 10.0, pt["curvature"])
+            ax.quiver(pt["point"][0], pt["point"][1], pt["gradient"][0], pt["gradient"][1], color=color, width=arrow_width)
+        else:
+            ax.quiver(pt["point"][0], pt["point"][1], pt["gradient"][0], pt["gradient"][1], width=arrow_width)
+
+for pt in sim.cbf.point_list:
+    ax.plot(pt["point"][0], pt["point"][1], 'o', color="green")
+    if "gradient" in pt.keys():
+        if "curvature" in pt.keys():
+            color = rgb(-10.0, 10.0, pt["curvature"])
+            ax.quiver(pt["point"][0], pt["point"][1], pt["gradient"][0], pt["gradient"][1], color=color, width=arrow_width)
+        else:
+            ax.quiver(pt["point"][0], pt["point"][1], pt["gradient"][0], pt["gradient"][1], width=arrow_width)
+
+# ----------------------------------------------------- Plotting ---------------------------------------------------------
 
 # manual_mode = True
-manual_mode = False
-
-'''
-Manually selects initial guesses
-'''
-if manual_mode:
-    pt = plt.ginput(1)
-    initial_guesses = [[ pt[0][0], pt[0][1] ]]
-
+manual_mode = True
+    
 '''
 Plots initial guesses
 '''
@@ -45,19 +56,40 @@ if not manual_mode:
         for j in range(res_y):
             pt = [ xv[i,j], yv[i,j] ]
             initial_guesses.append(pt)
-            plotSim.main_ax.plot( pt[0], pt[1], 'g.', alpha=0.3 )
+            ax.plot( pt[0], pt[1], 'g.', alpha=0.3 )
 
 '''
 Finds and plots the equilibrium points
 '''
-solutions, log = compute_equilibria_algorithm9(sim.plant, sim.clf, sim.cbf, initial_guesses, slack_gain=sim.p, clf_gain=sim.alpha)
+if manual_mode:
+    is_equilibrium = False
+    while not is_equilibrium:
+        print("Didn't hit the equilibrium...")
+        pt = plt.ginput(1)
+        clicked_pt = np.array([ pt[0][0], pt[0][1] ])
+        is_equilibrium, eq_pt = check_equilibrium(sim.plant, sim.clf, sim.cbf, clicked_pt, slack_gain=sim.p, clf_gain=sim.alpha)
+    solutions = [eq_pt]
+
+else:
+    solutions, log = compute_equilibria(sim.plant, sim.clf, sim.cbf, initial_guesses, slack_gain=sim.p, clf_gain=sim.alpha)
+    print("From " + str(log["num_trials"]) + " trials, algorithm converged " + str(log["num_success"]) + " times, and " + str(len(solutions)) + " solutions were found.")
+    print("Algorithm efficiency = " + str( log["num_success"]/log["num_trials"] ))
+
 num_sols = len(solutions)
-print("From " + str(log["num_trials"]) + " trials, algorithm converged " + str(log["num_success"]) + " times, and " + str(num_sols) + " solutions were found.")
-print("Algorithm efficiency = " + str( log["num_success"]/log["num_trials"] ))
 for k in range(num_sols):
     sol = solutions[k]
     print("Solution " + str(k+1) + " = " + str(sol))
-    plotSim.main_ax.plot( sol["x"][0], sol["x"][1], 'ro' )
+    ax.plot( sol["x"][0], sol["x"][1], 'ro' )
+
+    '''
+    Checks curvatures
+    '''
+    clf_curv = sim.clf.get_curvature(sol["x"])
+    cbf_curv = sim.cbf.get_curvature(sol["x"])
+
+    print("CLF curvature = " + str(clf_curv))
+    print("CBF curvature = " + str(cbf_curv))
+    print("Delta curvature = " + str(cbf_curv - clf_curv))
 
     if sol["stability"] > 0:
         print("Equilibrium point " + str(sol["x"]) + " is unstable, with value = " + str(sol["stability"]))

test_fitting.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from common import rgb
-from functions import Kernel, KernelBarrier
+from functions import Kernel, KernelLyapunov, KernelBarrier
+from controllers import compute_equilibria
+from dynamic_systems import ConservativeAffineSystem
 
 initial_state = [3.2, 3.0]
 initial_control = [0.0, 0.0]
@@ -16,40 +18,86 @@ kern_dim = kernel.kernel_dim
 print(kernel)
 print("Kernel dimension = " + str(kernel.kernel_dim))
 
-# ----------------------------------------------------- Define CBF ---------------------------------------------------------
-boundary_points = [ [-4.0, 0.0], [-4.0, -1.0], [-2.0, 0.5], [2.0, 0.5], [4.0, -1.0], [4.0, 0.0], [0.0, 1.0], [0.0, -0.5] ]   # (sad smile)
-# boundary_points = [ [-4.0, 0.0], [-4.0, 1.0], [-2.0, -0.5], [2.0, -0.5], [4.0, 1.0], [4.0, 0.0] ]                          # (bell shaped)
-
+# ---------------------------------------------------- Define CLF ----------------------------------------------------------
+base_level = 16.0
 points = []
-points += [ { "point": [ 2.0,  2.0], "level": 0.0, "gradient": [ 1.0,  1.0] } ]
-points += [ { "point": [ 2.0, -2.0], "level": 0.0, "gradient": [ 1.0, -1.0] } ]
-points += [ { "point": [-2.0, -2.0], "level": 0.0, "gradient": [-1.0, -1.0] } ]
-points += [ { "point": [-2.0,  2.0], "level": 0.0, "gradient": [-1.0,  1.0] } ]
-points += [ { "point": [ 0.0,  2.0], "level": 0.0, "gradient": [ 0.0,  1.0] } ]
-# points += [ { "point": [ 0.0, -2.0], "level": 0.0, "gradient": [ 0.0, -1.0] } ]
-# points += [ { "point": [ 2.0,  0.0], "level": 0.0, "gradient": [ 1.0,  0.0] } ]
-# points += [ { "point": [-2.0,  0.0], "level": 0.0, "gradient": [-1.0,  0.0] } ]
-displacement = 0*np.array([1,1])
+points += [{ "point": [ 0.0,  0.0], "level": 0.0 }]
+points += [{ "point": [ 3.0,  3.0], "level": base_level, "gradient": [ 1.0,  1.0] }]
+points += [{ "point": [-3.0,  3.0], "level": base_level, "gradient": [-1.0,  1.0] }]
+points += [{ "point": [ 0.0,  5.0],                      "gradient": [ 0.0,  1.0], "curvature": -1.6 }]
+
+clf = KernelLyapunov(*initial_state, kernel=kernel, points=points)
+
+# ----------------------------------------------------- Define CBF ---------------------------------------------------------
+points = []
+points += [{ "point": [ 2.0,  2.0], "level": 0.0, "gradient": [ 1.0,  1.0] }]
+points += [{ "point": [ 2.0, -2.0], "level": 0.0, "gradient": [ 1.0, -1.0] }]
+points += [{ "point": [-2.0, -2.0], "level": 0.0, "gradient": [-1.0, -1.0] }]
+points += [{ "point": [-2.0,  2.0], "level": 0.0, "gradient": [-1.0,  1.0] }]
+points += [{ "point": [ 0.0,  2.0], "level": 0.0, "gradient": [ 0.0,  1.0] }]
+points += [{ "point": [ 0.0, -2.0], "level": 0.0, "gradient": [ 0.0, -1.0] } ]
+points += [{ "point": [ 2.0,  0.0], "level": 0.0 }]
+points += [{ "point": [-2.0,  0.0], "level": 0.0 }]
+
+displacement = 3*np.array([0,1])
 for pt in points:
-    # pt.pop("gradient")
     pt["point"] = ( np.array(pt["point"]) + displacement ).tolist()
 
 cbf = KernelBarrier(*initial_state, kernel=kernel, points=points)
-# cbf = KernelBarrier(*initial_state, kernel=kernel, boundary_points=boundary_points)
 
-# cbf.fit(points)
-# cbf.define_boundary(boundary_points)
+# ----------------------------------------------------- Plotting ---------------------------------------------------------
+fig = plt.figure(constrained_layout=True)
+ax = fig.add_subplot(111)
+ax.set_title("Kernel-based CLF-CBF fitting")
 
-ax = cbf.plot(axeslim = [-10, 10, -10, 10])
-for pt in cbf.point_list:
+clf.plot_level(axes = ax, level = 26.1, axeslim = [-10, 10, -10, 10])
+cbf.plot_level(axes = ax, axeslim = [-10, 10, -10, 10])
 
-    plt.plot(pt["point"][0], pt["point"][1], 'go')
-
+arrow_width = 0.005
+for pt in clf.point_list:
+    ax.plot(pt["point"][0], pt["point"][1], 'o', color="blue")
     if "gradient" in pt.keys():
         if "curvature" in pt.keys():
             color = rgb(-10.0, 10.0, pt["curvature"])
-            plt.quiver(pt["point"][0], pt["point"][1], pt["gradient"][0], pt["gradient"][1], color=color)
+            ax.quiver(pt["point"][0], pt["point"][1], pt["gradient"][0], pt["gradient"][1], color=color, width=arrow_width)
         else:
-            plt.quiver(pt["point"][0], pt["point"][1], pt["gradient"][0], pt["gradient"][1])
+            ax.quiver(pt["point"][0], pt["point"][1], pt["gradient"][0], pt["gradient"][1], width=arrow_width)
+
+for pt in cbf.point_list:
+    ax.plot(pt["point"][0], pt["point"][1], 'o', color="green")
+    if "gradient" in pt.keys():
+        if "curvature" in pt.keys():
+            color = rgb(-10.0, 10.0, pt["curvature"])
+            ax.quiver(pt["point"][0], pt["point"][1], pt["gradient"][0], pt["gradient"][1], color=color, width=arrow_width)
+        else:
+            ax.quiver(pt["point"][0], pt["point"][1], pt["gradient"][0], pt["gradient"][1], width=arrow_width)
+
+# ---------------------------------------------- Define kernel function ----------------------------------------------------
+max_degree = 2
+kernel = Kernel(*initial_state, degree = max_degree)
+kern_dim = kernel.kernel_dim
+print(kernel)
+print("Dimension of kappa space = " + str(len(kernel.get_N_matrices())))
+
+# -------------------------------------------------- Define system ---------------------------------------------------------
+F = np.zeros([kern_dim,kern_dim])
+def g(state):
+    return np.eye(m)
+plant = ConservativeAffineSystem(initial_state=initial_state, initial_control=initial_control, kernel=kernel, F=F, g_method=g)
+
+# --------------------------------------------------- Verifying equilibrium ---------------------------------------------------------
+eq_pt = [0, 5]
+
+solutions, log = compute_equilibria(plant, clf, cbf, [eq_pt], slack_gain=1, clf_gain=1)
+
+for sol in solutions:
+    print("Solution was found = " + str(sol))
+    clf_curv = clf.get_curvature(sol["x"])
+    cbf_curv = cbf.get_curvature(sol["x"])
+
+print("Curvature of V at eq. = " + str( clf_curv ))
+print("Curvature of h at eq. = " + str( cbf_curv ))
+print("Diff between curvatures at eq. = " + str( cbf_curv - clf_curv ))
+
 
 plt.show()