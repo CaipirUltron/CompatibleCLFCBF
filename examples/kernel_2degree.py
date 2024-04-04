Fix bug in lowerbound on max Hessian eigenvalue, but still giving innacurate results

examples/kernel_2degree.py
@@ -40,7 +40,8 @@ clf_eig = np.array([ 1.0, 1.0 ])
 clf_angle = np.deg2rad(-45)
 Pquadratic = create_quadratic(eigen=clf_eig, R=rot2D(clf_angle), center=clf_center, kernel_dim=kernel_dim)
 
-clf = KernelLyapunov(*initial_state, kernel=kernel, P=load_compatible(__file__, Pquadratic, load_compatible=True))
+# clf = KernelLyapunov(*initial_state, kernel=kernel, P=load_compatible(__file__, Pquadratic, load_compatible=True))
+clf = KernelLyapunov(*initial_state, kernel=kernel, P=Pquadratic)
 # clf = KernelLyapunov(*initial_state, kernel=kernel, leading={ "shape": Pquadratic, "uses": ["lower_bound", "approximation"] })
 clf.is_SOS_convex(verbose=True)
 

examples/kernel_3degree.py
@@ -51,18 +51,17 @@ clf.is_SOS_convex(verbose=True)
 # Fits CBF to a U shaped obstacle
 cbf_center = [0.0, -0.0]
 
+# centers = [ [0.0, 0.0] ]
 centers = [ [-3.0, 0.0 ],[ 3.0, 0.0 ], [ -4.0, 3.0 ], [ 4.0, 3.0 ] ]
-# centers = [ [0.0, 0.0], [-3.0, 0.0 ],[ 3.0, 0.0 ], [ -4.0, 2.0 ], [ 4.0, 2.0 ] ]
-
 vertices = [ [ 5.0,-1.0 ], [ 5.0, 3.0 ], [ 3.0, 3.0 ], [ 3.0, 1.0 ],
              [-3.0, 1.0 ], [-3.0, 3.0 ], [-5.0, 3.0 ], [-5.0,-1.0 ] ]
 pts = polygon( vertices=vertices, spacing=0.5, closed=True, gradients=0, at_edge=False )
 
-cbf_eig = np.array([ 1.0, 1.0 ])
+cbf_eig = 0.01*np.array([ 1.0, 1.0 ])
 cbf_angle = np.deg2rad(0)
 Qquadratic = create_quadratic(eigen=cbf_eig, R=rot2D(cbf_angle), center=cbf_center, kernel_dim=kernel_dim)
+# cbf = KernelBarrier(*initial_state, kernel=kernel, boundary=pts, centers=centers, leading={ "shape": Qquadratic, "uses": ["approximation"] })
 cbf = KernelBarrier(*initial_state, kernel=kernel, boundary=pts, centers=centers, leading={ "shape": Qquadratic, "uses": ["approximation"] })
-# cbf = KernelBarrier(*initial_state, kernel=kernel, boundary=pts, centers=centers, leading={ "shape": Qquadratic, "uses": ["approximation"] } )
 cbf.is_SOS_convex(verbose=True)
 
 # ------------------------------------------------- Define controller ------------------------------------------------------

functions/functions.py
@@ -968,13 +968,6 @@ class KernelQuadratic(Function):
         A_list = self.kernel.get_A_matrices()
         return np.block([[ Ai.T @ self.matrix_coefs @ Aj + Aj.T @ Ai.T @ self.matrix_coefs for Aj in A_list ] for Ai in A_list ])
 
-    def compute_lowerbound_matrix(self, Pvar):
-        '''
-        Compute the matrix for the lowerbound on the maximum eigenvalue of the Hessian matrix
-        '''
-        As = self.kernel.Asum
-        return As.T @ Pvar @ As + As.T @ As.T @ Pvar
-
     def compute_lowerbound_slice(self):
         '''
         Compute the lower bound on the Hessian matrix
@@ -985,11 +978,24 @@ class KernelQuadratic(Function):
         for i in range(0,self.kernel_dim):
             half_down = symHbound[i:, :]
             if np.all( half_down == 0 ): break
-
+        
+        self.lowerbound_slice = ( slice(0,symHbound.shape[0]), slice(0,symHbound.shape[1]) )
         if np.all( half_down == 0 ):
             self.lowerbound_slice = ( slice(0,i), slice(0,i) )
         
-        self.lowerbound_slice = ( slice(0,symHbound.shape[0]), slice(0,symHbound.shape[1]) )
+    def compute_lowerbound_matrix(self, Pvar):
+        '''
+        Compute the matrix for the lowerbound on the maximum eigenvalue of the Hessian matrix
+        '''
+        As = self.kernel.Asum
+        return As.T @ Pvar @ As + As.T @ As.T @ Pvar
+
+    def compute_reduced_lowerbound_matrix(self, Pvar):
+        '''
+        Extract only the nonzero eigenvalues from the lowerbound matrix
+        '''
+        H = self.compute_lowerbound_matrix(Pvar)[self.lowerbound_slice]
+        return H + H.T
 
     def psd_constr(self):
         '''
@@ -1001,7 +1007,8 @@ class KernelQuadratic(Function):
         '''
         Returns corresponding non-negative definite Hessian constraint for input shape matrix
         '''
-        return self.compute_lowerbound_matrix(self.SHAPE)[self.lowerbound_slice] >> 0
+        # return cp.lambda_min( self.compute_reduced_lowerbound_matrix(self.SHAPE) ) >= 0.0
+        return self.compute_reduced_lowerbound_matrix(self.SHAPE) >> 0
         
     def is_SOS_convex(self, verbose=False):
         '''Returns True if the function is SOS convex'''
@@ -1033,7 +1040,7 @@ class KernelQuadratic(Function):
         '''
         n = self._dim
         A_list = self.kernel.get_A_matrices()
-
+        
         # Iterate over the input list to get problem requirements
         gradient_norms = []
         for pt in self.points:
@@ -1053,12 +1060,11 @@ class KernelQuadratic(Function):
 
             # Define point-level constraints
             if "level" in keys:
-                level_value = pt["level"]
-                if level_value >= -self.constant:
+                if pt["level"] >= -self.constant:
                     if self.fit_options["force_coords"] or pt["force_coord"]:
-                        self.constraints += [ 0.5 * m.T @ self.SHAPE @ m - self.constant == level_value ]
+                        self.constraints += [ 0.5 * m.T @ self.SHAPE @ m - self.constant == pt["level"] ]
                     else:
-                        self.cost += ( 0.5 * m.T @ self.SHAPE @ m - self.constant - level_value )**2
+                        self.cost += ( 0.5 * m.T @ self.SHAPE @ m - self.constant - pt["level"] )**2
                 else: continue
 
             # Define gradient constraints
@@ -1090,7 +1096,7 @@ class KernelQuadratic(Function):
                 self.cost += ( curvature_var - curvature )**2
 
         fit_problem = cp.Problem( cp.Minimize( self.cost ), self.constraints )
-        fit_problem.solve(verbose=False)
+        fit_problem.solve(verbose=False, max_iters = 100000)
 
         if "optimal" in fit_problem.status:
             if isinstance(self, KernelLyapunov):
@@ -1100,11 +1106,26 @@ class KernelQuadratic(Function):
             else:
                 print("Function fitting was successful with final cost = " + str(fit_problem.value) + " and message: " + str(fit_problem.status))
 
+            # Attempts regularization
             self.set_param( coefficients = self.SHAPE.value )
+            # if not np.all( np.linalg.eigvals( self.compute_reduced_lowerbound_matrix(self.SHAPE.value) ) > 0 ):
+            #     self.set_param( coefficients = self.regularize(self.SHAPE.value) )
+                
             return fit_problem
         else:
             raise Exception("Problem is " + fit_problem.status + ".")
-        
+
+    def regularize(self, Pnom):
+        '''
+        Attempts to regularize a given SHAPE to have only one global minimum
+        '''
+        reg_cost = cp.norm(self.SHAPE - Pnom)
+        reg_constraints = [ self.psd_constr(), self.non_nsd_Hessian_constr() ]
+        reg_problem = cp.Problem( cp.Minimize( reg_cost ), reg_constraints )
+        reg_problem.solve(verbose=True, max_iters = 100000)
+
+        return self.SHAPE.value
+
     def leading_function(self, Pleading, bound=0, approximate=False):
         '''
         Defines a leading function. Can be used as an lower bound, upper bound or as an approximation.

plot_triplet.py
@@ -27,7 +27,10 @@ if hasattr(sim, "pts"):
             gradient_vec = coords + np.array(pt["gradient"])
             ax.plot([ coords[0], gradient_vec[0]], [ coords[1], gradient_vec[1]], 'k-', alpha=0.6)
 
-contour_unsafe = sim.cbf.plot_levels(levels = [ -0.1*k for k in range(4,-1,-1) ], ax=ax, limits=limits)
+num_levels = 12
+contour_unsafe = sim.cbf.plot_levels(levels = [ -(0.5/num_levels)*k for k in range(num_levels-1,-1,-1) ], ax=ax, limits=limits)
+
+print(f"λ(lowerbound) = {np.linalg.eigvals(sim.cbf.compute_reduced_lowerbound_matrix(sim.cbf.Q))}\n")
 
 sim.kerneltriplet.plot_invariant(ax)
 sim.kerneltriplet.plot_attr(ax, "stable_equilibria", mcolors.BASE_COLORS["r"], 1.0)

test_lowerbound_maxeigenvalue.py
@@ -18,7 +18,7 @@ limits = 10*np.array([[-1, 1],[-1, 1]])
 ax.set_xlim(limits[0][0], limits[0][1])
 ax.set_ylim(limits[1][0], limits[1][1])
 
-kernel = Kernel(*initial_state, degree=2)
+kernel = Kernel(*initial_state, degree=3)
 p = kernel.kernel_dim
 Amatrices = kernel.get_A_matrices()
 
@@ -41,8 +41,10 @@ def extract_upper_block(Hsym):
     
     return ( slice(0,Hsym.shape[0]), slice(0,Hsym.shape[1]) )
 
-print( non_nsd_Hessian_constr(Psym) )
+print( non_nsd_Hessian_constr(Psym).shape )
+
 lowerbound_shape = extract_upper_block(non_nsd_Hessian_constr(Psym))
+print( non_nsd_Hessian_constr(Psym)[lowerbound_shape].shape )
 
 Pnom = cp.Parameter( (p,p), symmetric=True )
 Pvar = cp.Variable( (p,p), symmetric=True )